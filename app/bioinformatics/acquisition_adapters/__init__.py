c7f4ade26c059251424cf0ad22aae0a3ab6237b4
lichangda94-sys <lichangda94@gmail.com>
2026-05-21 22:41:03 +0800

    add Bioinformatics legacy candidate materialization gate

--- app/bioinformatics/acquisition_adapters/__init__.py
+++ app/bioinformatics/acquisition_adapters/__init__.py
@@ -13,16 +13,26 @@ from .standardized_bridge import (
     validate_legacy_standardized_asset_candidate,
     write_legacy_standardized_asset_candidates,
 )
+from .materialization import (
+    LEGACY_MATERIALIZATION_MANIFEST_VERSION,
+    build_legacy_candidate_materialization_plan,
+    materialize_legacy_standardized_asset_candidates,
+    validate_legacy_candidate_materialization_plan,
+)
 
 __all__ = [
     "LEGACY_ACQUISITION_SCHEMA_VERSION",
     "LEGACY_ASSET_CANDIDATE_SCHEMA_VERSION",
+    "LEGACY_MATERIALIZATION_MANIFEST_VERSION",
     "LegacyAcquisitionManifest",
     "adapt_geo_detection_manifest",
     "adapt_gtex_preview_manifest",
     "adapt_tcga_preview_manifest",
     "build_legacy_standardized_asset_candidates",
+    "build_legacy_candidate_materialization_plan",
+    "materialize_legacy_standardized_asset_candidates",
     "validate_legacy_acquisition_manifest",
+    "validate_legacy_candidate_materialization_plan",
     "validate_legacy_standardized_asset_candidate",
     "write_legacy_acquisition_manifest",
     "write_legacy_standardized_asset_candidates",
